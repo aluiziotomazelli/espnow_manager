18ed5eba3eefc241e39662259b4179da0107e1d4
Your Name <aluiziotomazelli@gmail.com>
2026-03-12 03:28:48 -0400

    test: boilerplate for test_tx_manager

---
+++ host_test/test_tx_manager/main/test_tx_manager.cpp
@@ -0,0 +1,14 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+// #include "mock_tx_state_machine.hpp" // Must be made
+#include "mock_wifi_hal.hpp"      // Already made
+#include "mock_message_codec.hpp" // Already made
+// #include "mock_channel_scanner.hpp"  // Must be made
+#include "tx_manager.hpp"
+
+// using ::testing::_;
+// using ::testing::DoAll;
+// using ::testing::NiceMock;
+// using ::testing::Return;
+// using ::testing::SetArgPointee;
