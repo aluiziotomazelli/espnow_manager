8f05e9501804c9d4950565d4fae8e49dc4a8d2fa
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-03-15 16:23:44 -0400

    ref: The MockMessageCodec has been updated to match the new IMessageCodec::encode   signature.

--- host_test/common/mock_message_codec.hpp
+++ host_test/common/mock_message_codec.hpp
@@ -8,9 +8,9 @@ class MockMessageCodec : public IMessageCodec
 {
 public:
     MOCK_METHOD(
-        std::vector<uint8_t>,
+        size_t,
         encode,
-        (const MessageHeader &header, const void *payload, size_t len),
+        (const MessageHeader &header, const void *payload, size_t len, uint8_t *out, size_t out_max),
         (override));
     MOCK_METHOD(std::optional<MessageHeader>, decode_header, (const uint8_t *data, size_t len), (override));
     MOCK_METHOD(bool, validate_crc, (const uint8_t *data, size_t len), (override));
