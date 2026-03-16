a38077afbf8a2345a8454954154d14babddbaeb6
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-03-15 22:50:00 -0400

    refactor: update init method signature and remove start_channel parameter from scan method

--- host_test/common/mock_discovery_manager.hpp
+++ host_test/common/mock_discovery_manager.hpp
@@ -10,8 +10,9 @@ public:
     MOCK_METHOD(
         esp_err_t,
         init,
-        (NodeId id, NodeType type, ITxManager &tx_mgr, IChannelObserver *observer),
+        (NodeId id, NodeType type, ITxManager *tx_mgr, IChannelObserver *observer),
         (override));
-    MOCK_METHOD(ScanResult, scan, (uint8_t start_channel), (override));
+    MOCK_METHOD(ScanResult, scan, (), (override));
     MOCK_METHOD(void, handle_probe, (const RxPacket &packet), (override));
+    MOCK_METHOD(void, set_channel, (uint8_t channel), (override));
 };
