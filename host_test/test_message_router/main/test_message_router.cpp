abd0c4d6266919623b10106eca2ff8956e175915
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-03-20 20:09:07 -0400

    test: boilerplate to message router tests

---
+++ host_test/test_message_router/main/test_message_router.cpp
@@ -0,0 +1,10 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "mock_discovery_manager.hpp"
+#include "mock_tx_manager.hpp"
+#include "mock_heartbeat_manager.hpp"
+#include "mock_pairing_manager.hpp"
+#include "mock_message_codec.hpp"
+
+#include "message_router.hpp"
