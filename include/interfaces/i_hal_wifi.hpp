674bebdd3c4e57e65d7339aa62c2395267d646d1
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-03-28 23:32:33 -0400

    chore(hal): Finalize separation and inline implementation for WiFi and ESP-NOW HALs, building on commit 155becd

--- include/interfaces/i_hal_wifi.hpp
+++ include/interfaces/i_hal_wifi.hpp
@@ -1,23 +1,18 @@
-// include/internface/i_hal_wifi.hpp
+// include/interfaces/i_hal_wifi.hpp
 #pragma once
 
 #include "esp_err.h"
-#include "freertos/FreeRTOS.h" // include FreeRTOS.h must appear in source files before include task.h
-#include "freertos/task.h"
-#include "esp_now.h"
+#include "esp_wifi_types.h"
 
 /**
  * @interface IWiFiHAL
- * @brief Hardware Abstraction Layer for WiFi and ESP-NOW drivers.
+ * @brief Hardware Abstraction Layer for WiFi driver.
  * @internal
  */
 class IWiFiHAL
 {
 public:
     virtual ~IWiFiHAL() = default;
-
-    /** @copydoc esp_wifi_set_channel() */
-    virtual esp_err_t wifi_set_channel(uint8_t channel) = 0;
 
     /** @copydoc esp_wifi_get_mode() */
     virtual esp_err_t wifi_get_mode(wifi_mode_t *mode) = 0;
@@ -27,28 +22,4 @@ public:
 
     /** @copydoc esp_wifi_get_channel() */
     virtual esp_err_t wifi_get_channel(uint8_t *primary, wifi_second_chan_t *second) = 0;
-
-    /** @copydoc esp_now_init() */
-    virtual esp_err_t hal_esp_now_init() = 0;
-
-    /** @copydoc esp_now_deinit() */
-    virtual esp_err_t hal_esp_now_deinit() = 0;
-
-    /** @copydoc esp_now_register_recv_cb() */
-    virtual esp_err_t hal_espnow_register_recv_cb(esp_now_recv_cb_t cb) = 0;
-
-    /** @copydoc esp_now_register_send_cb() */
-    virtual esp_err_t hal_espnow_register_send_cb(esp_now_send_cb_t cb) = 0;
-
-    /** @copydoc esp_now_add_peer() */
-    virtual esp_err_t hal_esp_now_add_peer(const esp_now_peer_info_t *peer) = 0;
-
-    /** @copydoc esp_now_mod_peer() */
-    virtual esp_err_t hal_esp_now_mod_peer(const esp_now_peer_info_t *peer) = 0;
-
-    /** @copydoc esp_now_del_peer() */
-    virtual esp_err_t hal_esp_now_del_peer(const uint8_t *peer_addr) = 0;
-
-    /** @copydoc esp_now_send() */
-    virtual esp_err_t hal_esp_now_send(const uint8_t *mac, const uint8_t *data, size_t len) = 0;
 };
