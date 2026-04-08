61812be192058900d7337f5985ce29e627c642fb
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-04-07 23:54:03 -0400

    feat: MockStorageManager load_stats and store_stats

--- host_test/common/mock_storage_manager.hpp
+++ host_test/common/mock_storage_manager.hpp
@@ -9,8 +9,10 @@
 class MockStorageManager : public IStorageManager
 {
 public:
-    MOCK_METHOD(esp_err_t, load_channel, (uint8_t &), (override));
+    MOCK_METHOD(esp_err_t, load_channel, (uint8_t&), (override));
     MOCK_METHOD(esp_err_t, store_channel, (uint8_t), (override));
-    MOCK_METHOD(esp_err_t, load_peers, (etl::ivector<PersistentPeer> &), (override));
-    MOCK_METHOD(esp_err_t, store_peers, (const etl::ivector<PersistentPeer> &, bool), (override));
+    MOCK_METHOD(esp_err_t, load_peers, (etl::ivector<PersistentPeer>&), (override));
+    MOCK_METHOD(esp_err_t, store_peers, (const etl::ivector<PersistentPeer>&, bool), (override));
+    MOCK_METHOD(esp_err_t, load_stats, (etl::ivector<PeerStatisticsPersist>&), (override));
+    MOCK_METHOD(esp_err_t, store_stats, (const etl::ivector<PeerStatisticsPersist>&), (override));
 };
