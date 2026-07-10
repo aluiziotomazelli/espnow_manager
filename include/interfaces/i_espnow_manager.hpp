694d3b20cf5c6d5b5351287eea500bf7f5814de7
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-07-10 18:16:43 -0400

    Revert "feat: add get_rx_queue accessor to IEspNowManager"
    This reverts commit d7ae47de0b7e66d3ec592ef650a8274bed9118db.

--- include/interfaces/i_espnow_manager.hpp
+++ include/interfaces/i_espnow_manager.hpp
@@ -371,12 +371,6 @@ public:
      * @note This method does not return errors.
      */
     virtual bool is_initialized() const = 0;
-
-    /**
-     * @brief Get the application RX queue handle.
-     * @return QueueHandle_t or nullptr if not initialized.
-     */
-    virtual QueueHandle_t get_rx_queue() const = 0;
 };
 
 } // namespace espnow
